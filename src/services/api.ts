491f19d412d065efa784c96358fdaac1f8b4b550
HamzaElshennawy <elshennawyhamza@gmail.com>
2025-08-17 21:34:51 +0300

    fix: update API_BASE_URL to use HTTPS

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,7 +8,7 @@ import {
   LoginResponse,
 } from "../types/api";
 
-const API_BASE_URL = " http://agrly.runasp.net";
+const API_BASE_URL = " https://agrly.runasp.net";
 
 class ApiService {
   private token: string | null = null;
